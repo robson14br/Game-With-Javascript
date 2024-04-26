9f06e5c77d8a9160542393220004a0a347ea5a35
Robson Dias <robson14br@outlook.com>
2024-04-26 16:30:58 -0300

    update

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
+const startButton = document.getElementById('startButton');
+const resetButton = document.getElementById('resetButton');
 
+
+let gameStarted = false; // Variável para controlar se o jogo começou ou não
+let score = 0; // Variável para armazenar o número de pontos
 
 const jump = (event) => {
     // Verifica se a tecla pressionada é a tecla de espaço (código 32)
-    if (event.keyCode === 32) {
+    if (gameStarted && event.keyCode === 32) { // Verifica se o jogo já começou e se a tecla de espaço foi pressionada
         mario.classList.add('jump');
         setTimeout(() => {
             mario.classList.remove('jump');
@@ -13,6 +18,7 @@ const jump = (event) => {
 }
 
 const loop = setInterval(() => {
+    if (!gameStarted) return; // Se o jogo não começou, não executa o loop
     const pipePosition = pipe.offsetLeft;
     const marioPosition = Number(window.getComputedStyle(mario).bottom.replace('px', '') );
     
@@ -28,10 +34,24 @@ const loop = setInterval(() => {
         mario.style.width = '60px'
         mario.style.marginLeft = '65px'
         clearInterval(loop)
-    } 
+        resetButton.style.display = 'inline'; // Exibe o botão de reset quando o jogador perde
+        resetButton.addEventListener('click', function() {
+            location.reload();
+        });
+
+    } else if (pipePosition === 0) {
+        score += 100 // Incrementa o número de pontos a cada cano pulado
+        document.getElementById('score').innerText = `Pontuação: ${score}`;
+    }
     
 }, 10);
 
 
 
 document.addEventListener('keydown', jump);
+
+startButton.addEventListener('click', () => {
+    gameStarted = true; // Define que o jogo começou quando o botão é clicado
+    startButton.style.display = 'none'; // Esconde o botão de início
+    gameLoop(); // Inicia o loop do jogo
+});
